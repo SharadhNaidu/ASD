64bb0cd9529dfafd06fedd4aeca346dcd0e24170
SharadhNaidu <sharadhanaidu@rvu.edu.in>
2026-05-25 06:18:39 +0530

    io

---
+++ Day 1/io.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    cout << "Yo I am using C++" << endl;
+    cout << "This is my second line" << endl << "wait this is so cool" << "\n" << "\"\\n\" is default in any language ig " << endl ;
+    cout << "Dont Stalk what i am learning you bafoon , I have my site trackers on every repo Ill get to know if anyone stalks me." <<endl;
+    int x ;
+    cin >> x;
+    cout << x  << endl;
+    int z , y;
+    cin >> z >> y;
+    cout << z + y << " This is the sum of " << z << " and " << y << " .";
+    
+    //this is a comment looks cool
+    /*This is a multiline one , this is what I do most of the time to the code you write  */
+    long a = 10;
+    cout << a  << endl;
+    return 0;
+}
