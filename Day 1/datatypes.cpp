03139fcdbadcbfbc72f5e609f81e8568f08e6b1f
SharadhNaidu <sharadhanaidu@rvu.edu.in>
2026-05-25 06:37:05 +0530

    datatypes

---
+++ Day 1/datatypes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+//int , long long , float , double , string , char , long double
+int x = 0;
+long y = 0;
+float f ;
+double d ;
+string str;
+char ch;
+//String takes characters which only appear before the so inorder to take input of an entire line with spaces we have to use getline(cin,var);
+cin >> str >> ch;
+cin >> x >> y ;
+cout << x << y << str << ch;
+}
